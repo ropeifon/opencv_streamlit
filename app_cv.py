d760fb5c7d76a821971498a0a1b6317a08214ed2
Roberto Peixoto <robertopeixotofonseca18@gmail.com>
2024-02-27 09:40:44 -0300

    Primeiro commit

---
+++ app_cv.py
@@ -0,0 +1,74 @@
+import cv2 
+import streamlit as st
+from PIL import Image
+import numpy as np
+from skimage import morphology, io, color, feature, filters
+
+def brilho_imagem(imagem, resultado):
+	img_brilho = cv2.convertScaleAbs(imagem, beta = resultado)
+	return img_brilho
+
+def borra_imagem(imagem, resultado):
+	img_borrada = cv2.GaussianBlur(imagem, (7, 7), resultado)
+	return img_borrada 
+
+def melhora_detalhe(imagem):
+	img_melhorada = cv2.detailEnhance(imagem, sigma_s = 34, sigma_r = 0.50)
+	return img_melhorada 
+
+def escala_cinza(imagem):
+	img_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+	return img_cinza
+
+def principal():
+	st.title('Open CV Data App')
+	st.subheader('Este aplicativo web permite integrar processamento de imagens com OpenCV')
+	st.text('Streamlit com OpenCV')
+	arquivo_imagem = st.file_uploader('Envie a sua imagem', type = ['jpg', 'png', 'jpeg'])
+	
+	taxa_borrao = st.sidebar.slider('Borrao', min_value = 0.2, max_value = 3.5)
+	qtd_brilho = st.sidebar.slider('Brilho', min_value = -50, max_value = 50, value = 0)
+	filtro_aprimoramento = st.sidebar.checkbox('Melhorar detalhes da imagem')
+	filtro_cinza = st.sidebar.checkbox('Convertar para Escala de cinza')
+	img_erosao = st.sidebar.checkbox('Filtro erosão')
+	img_dilatacao = st.sidebar.checkbox('Filtro dilatacao')
+	img_edge = st.sidebar.checkbox('Filtro Edge')
+
+
+
+
+	if not arquivo_imagem:
+		return None
+
+	imagem_original = Image.open(arquivo_imagem)
+	imagem_original = np.array(imagem_original)
+
+	imagem_processada = borra_imagem(imagem_original, taxa_borrao)
+	imagem_processada = brilho_imagem(imagem_processada, qtd_brilho)
+
+	if filtro_aprimoramento:
+		imagem_processada = melhora_detalhe(imagem_processada)
+
+
+	if filtro_cinza:
+		imagem_processada = escala_cinza(imagem_processada)
+
+	if img_erosao:
+		imagem_processada = morphology.erosion(imagem_processada)
+
+	if img_dilatacao:
+		imagem_processada = morphology.dilation(imagem_processada)
+
+	if img_edge:
+		imagem_processada = filters.sobel(imagem_processada)
+
+
+
+
+
+
+	st.text('Imagem Original vs Imagem Processada')
+	st.image([imagem_original, imagem_processada])
+
+if __name__ == '__main__':
+	principal()
